87cf89f2acf3a4f6232c41b365e4184a10ac65c6
Ivaylo2017 <ivoaleksiev@yahoo.com>
2023-04-18 01:55:38 +0000

    Add Application and refactor Server to handle processes and state

--- dictionary/lib/runtime/server.ex
+++ dictionary/lib/runtime/server.ex
@@ -1,12 +1,14 @@
 defmodule Dictionary.Runtime.Server do
   @type t :: pid()
+
+  @me __MODULE__
   alias Dictionary.Impl.WordList
 
   def start_link do
-    {:ok, pid} = Agent.start_link(&WordList.word_list/0)
+    {:ok, pid} = Agent.start_link(&WordList.word_list/0, name: @me)
   end
 
-  def random_word(pid) do
-    Agent.get(pid, &WordList.random_word/1)
+  def random_word() do
+    Agent.get(@me, &WordList.random_word/1)
   end
 end
